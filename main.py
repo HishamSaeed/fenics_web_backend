c0e46be2ee9d52f293d65c347db587e77cccd682
HishamSaeed <hisham.saeed.92@gmail.com>
2024-03-16 15:31:57 +0100

    added host

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import jsonify, request
 from config import app
 
-
 @app.route("/")
 def home():
-    print("Hello world")
-    return 'Hello, world! , this is just a demo for flask . This is a demo for fenicsWeb, test CORS'
+    return 'Hello, world! , this is just a demo for flask . This is a demo for fenicsWeb, test CORS main program'
+
+@app.route('/t_start', methods=["GET"])
+def getTStart():
+    response = jsonify({'tStart': 742}) 
+    return response
 
 if __name__ == "__main__":
-    ##with app.app_context():
-    ##    db.create_all()
-    app.run(debug=True)    
-    ## socketIo.run(app=app, debug=True)
+    app.run(debug=True, host='0.0.0.0')    
